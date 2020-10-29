99634d7d14bcea38c72320565207b9e472f7aa3f
Nicholas Trujillo <nicholasrtrujillo@gmail.com>
2020-10-28 23:13:18 -0700

    add initial scaffold

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,49 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
 import Landing from "./components/layout/Landing";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import { Counter } from "./features/counter/Counter";
+import Routes from "./components/routing/Routes";
+import { LOGOUT } from "./actions/types";
+
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
+import { loadUser } from "./actions/auth";
+import setAuthToken from "./utils/setAuthToken";
 
 import "./App.css";
 
-function App() {
+const App = () => {
+  useEffect(() => {
+    // check for token in LS
+    if (localStorage.token) {
+      setAuthToken(localStorage.token);
+    }
+    store.dispatch(loadUser());
+
+    // log user out from all tabs if they log out in one tab
+    window.addEventListener("storage", () => {
+      if (!localStorage.token) store.dispatch({ type: LOGOUT });
+    });
+  }, []);
+
   return (
-    <Router>
-      <>
-        <Navbar />
-        <Route exact path='/'>
-          <Landing />
-        </Route>
-        <section className='container'>
+    <Provider store={store}>
+      <Router>
+        <>
+          <Navbar />
           <Switch>
-            <Route exact path='/register'>
-              <Register />
+            <Route exact path='/'>
+              <Landing />
             </Route>
-            <Route exact path='/login'>
-              <Login />
+            <Route>
+              <Routes />
             </Route>
           </Switch>
-        </section>
-      </>
-    </Router>
+        </>
+      </Router>
+    </Provider>
   );
-}
+};
 
 export default App;
--- client/src/app/store.js
+++ client/src/app/store.js
@@ -1,8 +1,13 @@
-import { configureStore } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "../features/counter/counterSlice";
+import rootReducer from "../reducers";
+
+const initialState = {};
 
 export default configureStore({
   reducer: {
     counter: counterReducer,
+    rootReducer,
+    initialState,
   },
 });
